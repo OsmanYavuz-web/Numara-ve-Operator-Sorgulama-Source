95ef89c138e3a70871c19b80f2104b23780ec717
Osman Yavuz <58117722+OsmanYavuz-web@users.noreply.github.com>
2020-03-12 10:42:39 +0300

    Update

---
+++ Numara Sorgulama/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace Numara_Sorgulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.programıKapatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.programHakkındaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(9, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Numara";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 54);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBox1.MaxLength = 11;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(11, 141);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(105, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 171);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 28);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Numarayı Sorgula";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(11, 99);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(105, 35);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(19, 535);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(260, 22);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "http://www.turktuccar.com/numara-sorgula";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(11, 464);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(21, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(267, 66);
+            this.webBrowser1.TabIndex = 9;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(9, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Güvenlik Kodu";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Location = new System.Drawing.Point(123, 99);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(49, 35);
+            this.linkLabel1.TabIndex = 12;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Kodu Yenile";
+            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(11, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(334, 18);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Bekleniyor..";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(183, 171);
+            this.button2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 28);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Yeni Sorgu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Maroon;
+            this.label4.Location = new System.Drawing.Point(9, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 16);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Numara Sahibi";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(179, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(171, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Not: 0 koymayı unutmayınız.";
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Location = new System.Drawing.Point(285, 464);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(21, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(350, 38);
+            this.webBrowser2.TabIndex = 17;
+            this.webBrowser2.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser2_DocumentCompleted);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(286, 508);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(250, 22);
+            this.textBox4.TabIndex = 18;
+            this.textBox4.Text = "https://www.turkiye.gov.tr/btk-numara-tasima";
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(11, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(334, 42);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Bekleniyor..";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(286, 535);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(250, 22);
+            this.textBox6.TabIndex = 23;
+            this.textBox6.Text = "<DIV class=reminderContainer>";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.Maroon;
+            this.label7.Location = new System.Drawing.Point(9, 243);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(116, 16);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "Operatör Bilgisi";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(542, 508);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(93, 50);
+            this.textBox5.TabIndex = 25;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.programıKapatToolStripMenuItem,
+            this.programHakkındaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(361, 24);
+            this.menuStrip1.TabIndex = 26;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // programıKapatToolStripMenuItem
+            // 
+            this.programıKapatToolStripMenuItem.Name = "programıKapatToolStripMenuItem";
+            this.programıKapatToolStripMenuItem.Size = new System.Drawing.Size(101, 20);
+            this.programıKapatToolStripMenuItem.Text = "Programı Kapat";
+            this.programıKapatToolStripMenuItem.Click += new System.EventHandler(this.programıKapatToolStripMenuItem_Click);
+            // 
+            // programHakkındaToolStripMenuItem
+            // 
+            this.programHakkındaToolStripMenuItem.Name = "programHakkındaToolStripMenuItem";
+            this.programHakkındaToolStripMenuItem.Size = new System.Drawing.Size(118, 20);
+            this.programHakkındaToolStripMenuItem.Text = "Program Hakkında";
+            this.programHakkındaToolStripMenuItem.Click += new System.EventHandler(this.programHakkındaToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(361, 313);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.webBrowser2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Numara Sorgulama | Osman Yavuz";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem programıKapatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem programHakkındaToolStripMenuItem;
+    }
+}
+
---
+++ Numara Sorgulama/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using mshtml;
+
+namespace Numara_Sorgulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string kaynakKod = null;
+        string kaynakKod2 = null;
+
+        #region FORM_LOAD
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           webBrowser1.Navigate(textBox3.Text);
+           webBrowser2.Navigate(textBox4.Text);
+        }
+        #endregion
+
+        #region Captcha Alma
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            try
+            {
+                textBox2.Clear();
+
+                kaynakKod = webBrowser1.Document.Body.InnerHtml.ToString(); // site kaynak kod
+
+                IHTMLDocument2 doc = (IHTMLDocument2)webBrowser1.Document.DomDocument; // unmanaged document nesnesini alıyoruz
+                IHTMLControlRange imgler = (IHTMLControlRange)((HTMLBody)doc.body).createControlRange(); // controlRange ile Html nesne dizisi oluşturuyoruz
+
+                foreach (IHTMLImgElement img in doc.images) // Tüm img elementleri için
+                {
+                    imgler.add((IHTMLControlElement)img); // Koleksiyona elementi ekliyoruz
+                    imgler.execCommand("copy", false, null); // Koleksiyonu Clipboard a kopyalıyoruz
+
+                    using (Bitmap bmp = (Bitmap)Clipboard.GetDataObject().GetData(DataFormats.Bitmap)) // Clipboard daki resmi bitmap olarak alıyoruz
+                    {
+                        pictureBox1.Image = Image.FromHbitmap(bmp.GetHbitmap()); // Picturebox ta gösteriyoruz
+                    }
+                    break;
+                }
+            }
+            catch { }
+        }
+        #endregion
+
+        #region Sorgu gönderme
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Bekleniyor..";
+            label6.Text = "Bekleniyor..";
+
+            label3.ForeColor = Color.Black;
+            label6.ForeColor = Color.Black;
+
+            try
+            {
+                #region Numara Sorgulama
+                if (textBox1.Text == "" || textBox2.Text == "")
+                {
+                    MessageBox.Show("Tüm alanları doldurmalısınız!", "Hata;", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else if (textBox1.TextLength == 1 || textBox1.TextLength < 0)
+                {
+                    MessageBox.Show("Telefon numarası 11 haneli olmalıdır.", "Hata;", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    webBrowser1.Document.GetElementById("no").InnerText = textBox1.Text;
+                    webBrowser1.Document.GetElementById("guvkod").InnerText = textBox2.Text;
+                    HtmlElementCollection elc = this.webBrowser1.Document.GetElementsByTagName("input");
+                    foreach (HtmlElement el in elc)
+                    {
+                        if (el.GetAttribute("value").Equals("Numarayı Sorgula"))
+                        {
+                            el.InvokeMember("Click");
+                        }
+                    }
+                }
+                #endregion
+
+                #region Operatör sorgulama
+                string numara = textBox1.Text;
+                numara = numara.Remove(0, 1);
+                webBrowser2.Document.GetElementById("txtMsisdn").InnerText = numara;
+                HtmlElementCollection elc2 = this.webBrowser2.Document.GetElementsByTagName("input");
+                foreach (HtmlElement el2 in elc2)
+                {
+                    if (el2.GetAttribute("value").Equals("Sorgula"))
+                    {
+                        el2.InvokeMember("Click");
+                    }
+                }
+                #endregion
+            }
+            catch { }
+        }
+        #endregion
+
+        #region Kod Yenileme
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+        #endregion
+
+        #region Yeni Sorgu
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                webBrowser1.Refresh();
+                webBrowser2.Navigate(textBox4.Text);
+                textBox1.Text = "0";
+                textBox2.Clear();
+                label3.Text = "Bekleniyor..";
+                label6.Text = "Bekleniyor..";
+
+                label3.ForeColor = Color.Black;
+                label6.ForeColor = Color.Black;
+            }
+            catch { }
+        }
+        #endregion
+
+        #region Kontrol İşlemleri
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+              #region Numara sorgulama
+                var links = webBrowser1.Document.GetElementsByTagName("div");
+                foreach (HtmlElement link in links)
+                {
+                    if (link.GetAttribute("id") == "sonuc")
+                    {
+                        label3.Text = link.InnerText;
+                    }
+                }
+
+                if (label3.Text.IndexOf("Güvenlik Kodu Yanlış") != -1)
+                {
+                    webBrowser1.Refresh();
+                    label3.ForeColor = Color.DarkRed;
+                    label3.Text = "Güvenlik Kodu Yanlış";
+                    textBox2.Clear();
+                        
+                }
+                else if (label3.Text.IndexOf("Numara Bulunamadı") != -1)
+                {
+                    webBrowser1.Refresh();
+                    label3.ForeColor = Color.DarkRed;
+                    label3.Text = "Numara Bulunamadı";
+                    textBox2.Clear();
+                }
+                else
+                {
+                    label3.ForeColor = Color.DarkBlue;
+                }
+                #endregion
+
+              #region Operatör sorgulama
+                if (kaynakKod2.IndexOf("hizmet aldığı işletmeci") != -1)
+                {
+                    label6.ForeColor = Color.DarkBlue;
+                    string gelen = kaynakKod2;
+                    int titleIndexBaslangici = gelen.IndexOf(textBox6.Text) + textBox6.TextLength;
+                    int titleIndexBitisi = gelen.Substring(titleIndexBaslangici).IndexOf("</DIV>");
+                    string cikti = gelen.Substring(titleIndexBaslangici, titleIndexBitisi).Remove(0, 11).Replace("numarası", "Numara");
+                    label6.Text = cikti.Replace("hizmet aldığı işletmeci: ","");
+                }
+                else
+                {
+                    label6.Text = "Bekleniyor..";
+                    label6.ForeColor = Color.Black;
+                }
+
+                if (kaynakKod2.IndexOf("numarası bir işletmeciye ait değildir.") != -1)
+                {
+                    label6.Text = "Bu numara herhangi bir işletmeciye ait değildir";
+                }
+              #endregion
+            }
+            catch { }
+        }
+        #endregion
+
+        #region Operatör sorgulama site kaynak kod
+        private void webBrowser2_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            try
+            {
+                kaynakKod2 = webBrowser2.Document.Body.InnerHtml.ToString(); // site kaynak kod
+                textBox5.Text = kaynakKod2;
+            }
+            catch { }
+        }
+        #endregion
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void programıKapatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void programHakkındaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 frm2 = new Form2();
+            frm2.Show();
+        }
+
+    }
+}
